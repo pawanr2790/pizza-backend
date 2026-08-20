18c08a0bf2614d050dae8d4d85ff3777c87cd3da
pawan <pawanr2790@gmail.com>
2026-08-20 13:44:31 +0530

    adding passport

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -213,7 +213,6 @@ export const googleCallback = (req, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
